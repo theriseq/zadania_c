c25cca3c2ff27283596509394422d6bf3c05c01c
Patryk Orłowski <64466611+theriseq@users.noreply.github.com>
2021-01-06 16:42:55 +0100

    Update projekt2.c

--- projekt2.c
+++ projekt2.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 #define MAX_L 50
 #define MAX_POST 40
 
@@ -15,14 +16,27 @@ postac wszyscy[MAX_POST];
 char nazwisko[MAX_L];
 char imie[MAX_L];
 char data_urodzenia[MAX_L];
-int ile = 0;
+int ile = 0; //zmienna przechowuje ilosc poprawnie wpisanych danych
 
-char data[MAX_L];
-char sep[] = "-"; // separuje date
-char *ptr;
-
+bool czy_wprowadzone_dane_ok (char *data) {
+    char miesiac[50];
+    memset(miesiac, 0, sizeof(miesiac));
+    if (strlen(data) == 10 && data_urodzenia[4] == '-' && data_urodzenia[7] == '-') {
+        strncat(miesiac, &data[5], 1);
+        strncat(miesiac, &data[6], 1);
+        //printf("mieisac %s", miesiac);
+        // zrob dzien
+        return true;
+    } else {
+        return false;
+    }
+}
 
 void pobierz_dane() {
+    char data[MAX_L];
+    char sep[] = "-"; //separator
+    char *ptr;
+
     for (int i = 0; i < 39; i++) {
         //pobierz dane
         printf("Podaj nazwisko: ");
@@ -36,23 +50,37 @@ void pobierz_dane() {
         gets(imie);
         printf("\nPodaj date: ");
         gets(data_urodzenia);
+        //strlen(data_urodzenia) < 10 ||
+        //        data_urodzenia[4] != '-' ||
+        //        data_urodzenia[7] != '-' ||
+        //        data_urodzenia[6] - '0' > 2 ||
+        //        (data_urodzenia[8] - '0' >= 3  && data_urodzenia[9] >= 2)
+        if (czy_wprowadzone_dane_ok(data_urodzenia) == false) {
+            printf("\nNie podano prawidlowej daty. Nie dodaje postaci do struktury.\n");
+            if ( data_urodzenia[6] - '0' > 2) {
+                printf("Bledny miesiac\n");
+            }
+            if (data_urodzenia[8] - '0' >= 3  && data_urodzenia[9] >= 2) {
+                printf("bledny dzien\n");
+            }
+        } else {
+            printf("Dodaje %s %s %s..\n", nazwisko, imie, data_urodzenia);
+            //przypisz dane do struktury
+            strcpy(wszyscy[ile].nazwisko, nazwisko);
+            strcpy(wszyscy[ile].imie, imie);
+            strcpy(wszyscy[ile].data_urodzenia, data_urodzenia);
 
-        //przypisz dane do struktury
-        strcpy(wszyscy[i].nazwisko, nazwisko);
-        strcpy(wszyscy[i].imie, imie);
-        strcpy(wszyscy[i].data_urodzenia, data_urodzenia);
+            ptr = strtok(wszyscy[ile].data_urodzenia, sep);
+            memset(data, 0, sizeof(data));
+            while (ptr != NULL) {
+                strcat(data, ptr);
+                ptr = strtok(NULL, sep);
+            }
+            wszyscy[ile].data_uro_int = atoi(data);
+            memset(data, 0, sizeof(data));
 
-        ptr = strtok(wszyscy[i].data_urodzenia, sep);
-        memset(data, 0, sizeof(data));
-        while (ptr != NULL) {
-            strcat(data, ptr);
-            //printf("data ptr\n: %s\n", data);
-            ptr = strtok(NULL, sep);
+            ile++;
         }
-        wszyscy[i].data_uro_int = atoi(data);
-        memset(data, 0, sizeof(data));
-
-        ile++;
     }
 }
 void posortuj_dane() {
@@ -69,8 +97,7 @@ void posortuj_dane() {
 }
 void wypisz_dane() {
     if (ile > 0) {
-        system("cls");
-        printf("\n---- POSORTOWANE DANE----\n");
+        printf("\n---- POSORTOWANE DANE ----\n");
         for (int i = 0; i < ile; i++) {
             printf("%i. %s %s\n", i+1, wszyscy[i].nazwisko, wszyscy[i].imie);
         }
